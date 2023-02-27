b78e231b9f5ba90cffc2dbc49e606725d01bb7a2
LizaPadawan <lizaPadawan@yandex.ru>
2023-02-27 14:49:31 +0300

    Liza: selector order problem

--- src/services/selectors.js
+++ src/services/selectors.js
@@ -8,5 +8,4 @@ export const ingredientsSelector = (state) => state.ingredientsReduser
 export const constructorSelector = (state) => state.constructorReduser
 export const openModalSelector = (state) => state.modalReduser
 export const currentIngredientSelector = (state) => state.currentIngredientReduser
-export const currentOrderSelector = (state) => {console.log(state);
-    return state.orderReduser}
+export const currentOrderSelector = (state) => state.orderReducer
