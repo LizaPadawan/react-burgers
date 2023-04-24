328839f8b3d7569dd9984f672f2e9e2340a77cbf
Liza <gubanova-ea@yandex.ru>
2023-04-24 18:01:55 +0300

    Liza: middle

--- src/services/actions/constructor-actions-creator.ts
+++ src/services/actions/constructor-actions-creator.ts
@@ -1,7 +1,16 @@
 import { SET_CONSTRUCTOR, CLEAN_CONSTRUCTOR } from "../action-types";
 import { TIngredient } from "../../components/ingredients-proptypes";
 
+export interface ISetConstructorAction {
+    readonly type: typeof SET_CONSTRUCTOR;
+    readonly payload: Array<TIngredient>
+}
+
+export interface ICleanConstructorAction {
+    readonly type: typeof CLEAN_CONSTRUCTOR;
+}
+
 export const constructorActions = {
-    setConstructor: (data : Array<TIngredient>) => ({type: SET_CONSTRUCTOR, payload: data}),
+    setConstructor: (data : Array<TIngredient>) :  ISetConstructorAction => ({type: SET_CONSTRUCTOR, payload: data}),
     cleanConstructor: () => ({type: CLEAN_CONSTRUCTOR})
 }
--- src/services/actions/ingredients-actions-creator.ts
+++ src/services/actions/ingredients-actions-creator.ts
@@ -1,8 +1,17 @@
 import { FETCH_INGREDIENTS_REQUEST, FETCH_INGREDIENTS_SUCCESS, FETCH_INGREDIENTS_ERROR } from "../action-types";
 import { TIngredient } from "../../components/ingredients-proptypes";
 
+export interface ISetConstructorAction {
+    readonly type: typeof SET_CONSTRUCTOR;
+    readonly payload: Array<TIngredient>
+}
+
+export interface IFetchIngredientRequestAction {
+    readonly type: typeof FETCH_INGREDIENTS_REQUEST;
+}
+
 export const ingredientsActions = {
-	requestIngredients: () => ({type: FETCH_INGREDIENTS_REQUEST}),
+	requestIngredients: () : IFetchIngredientRequestAction => ({type: FETCH_INGREDIENTS_REQUEST}),
     setIngredients: (data : Array<TIngredient>) => ({type: FETCH_INGREDIENTS_SUCCESS, payload: data}),
     initialIngredients: () => ({type: FETCH_INGREDIENTS_ERROR})
 }
